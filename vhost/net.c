36b314a238666c39d4fcee6d7d84f8124110e6e1
kkndyu <kkndyu@gmail.com>
2016-10-25 18:08:31 +0800

    hook vhost tx, display pkt content

--- vhost/net.c
+++ vhost/net.c
@@ -329,8 +329,39 @@ static void vhost_zerocopy_callback(struct ubuf_info *ubuf, bool success)
 	rcu_read_unlock_bh();
 }
 
-/* Expects to be always run from workqueue - which acts as
- * read-size critical section for our kind of RCU. */
+void HexDump(char *buf,int len,int addr) {
+    int i,j,k;
+    char binstr[80];
+
+    for (i=0;i<len;i++) {
+        if (0==(i%16)) {
+            sprintf(binstr,"%08x -",i+addr);
+            sprintf(binstr,"%s %02x",binstr,(unsigned char)buf[i]);
+        } else if (15==(i%16)) {
+            sprintf(binstr,"%s %02x",binstr,(unsigned char)buf[i]);
+            sprintf(binstr,"%s  ",binstr);
+            for (j=i-15;j<=i;j++) {
+                sprintf(binstr,"%s%c",binstr,('!'<buf[j]&&buf[j]<='~')?buf[j]:'.');
+            }
+            printk("%s\n",binstr);
+        } else {
+            sprintf(binstr,"%s %02x",binstr,(unsigned char)buf[i]);
+        }
+    }
+    if (0!=(i%16)) {
+        k=16-(i%16);
+        for (j=0;j<k;j++) {
+            sprintf(binstr,"%s   ",binstr);
+        }
+        sprintf(binstr,"%s  ",binstr);
+        k=16-k;
+        for (j=i-k;j<i;j++) {
+            sprintf(binstr,"%s%c",binstr,('!'<buf[j]&&buf[j]<='~')?buf[j]:'.');
+        }
+        printk("%s\n",binstr);
+    }
+}
+
 static void handle_tx(struct vhost_net *net)
 {
 	struct vhost_net_virtqueue *nvq = &net->vqs[VHOST_NET_VQ_TX];
@@ -394,6 +425,12 @@ static void handle_tx(struct vhost_net *net)
 			       "out %d, int %d\n", out, in);
 			break;
 		}
+        /* print test pkt content */
+        printk("0x%p===%d===%d\n",vq->iov->iov_base, iov_length(vq->iov, out), out);
+        HexDump(vq->iov->iov_base, 100, (int)(vq->iov->iov_base));
+        if(out==2){
+            HexDump((vq->iov[1]).iov_base, 100, (int)((vq->iov[1]).iov_base));
+        }
 		/* Skip header. TODO: support TSO. */
 		s = move_iovec_hdr(vq->iov, nvq->hdr, hdr_size, out);
 		msg.msg_iovlen = out;
